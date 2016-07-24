8e55f615ea41260c4c8f47abed42addf923cb7f9
Amreeta Choudhury <amreeta89@yahoo.com>
2016-07-23 22:44:54 -0400

    Fixed drop down issue, added content to html pages and routing

---
+++ server.js
@@ -0,0 +1,12 @@
+//Dependencies
+
+var htmlRoutes = require('.app/routing/html-routes.js');
+var apiRoutes = require('.app/routing/api-routes.js');
+
+function friendFinder() {
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+}
